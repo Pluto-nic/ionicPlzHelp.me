518bfa8b0268557021a1b0f2c514376add8a9f4b
Kevin Chiang <kevinwchiang@gmail.com>
2015-09-12 12:32:12 -0700

    auth0 implemented

--- www/js/app.js
+++ www/js/app.js
@@ -4,9 +4,11 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'auth0', 
+  'angular-storage', 'angular-jwt'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, auth) {
+  auth.hookEvents();
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -22,15 +24,25 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
 
+.config(function($stateProvider, $urlRouterProvider, authProvider, $httpProvider,
+  jwtInterceptorProvider) {
+  $stateProvider
+  // This is the state where you'll show the login
+  .state('login', { // Notice: this state name matches the loginState property value to set in authProvider.init({...}) below...
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl',
+  })
   // this will route the app to menu.html when clicking on side menu
-    .state('app', {
+  .state('app', {
     url: '/app',
     abstract: true,
     templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
+    controller: 'AppCtrl',
+    data: {
+      requiresLogin: true
+    }
   })
 
   .state('app.search', {
@@ -39,6 +51,9 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       'menuContent': {
         templateUrl: 'templates/search.html'
       }
+    },
+    data: {
+      requiresLogin: true
     }
   })
   .state('app.profile', {
@@ -48,6 +63,9 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         templateUrl: 'templates/profile.html',
         controller: 'ProfileCtrl'
       }
+    },
+    data: {
+      requiresLogin: true
     }
   })
   .state('app.settings', {
@@ -56,6 +74,9 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         'menuContent': {
           templateUrl: 'templates/settings.html'
         }
+      },
+      data: {
+        requiresLogin: true
       }
     })
     .state('app.playlists', {
@@ -65,9 +86,22 @@ angular.module('starter', ['ionic', 'starter.controllers'])
           templateUrl: 'templates/playlists.html',
           controller: 'PlaylistsCtrl'
         }
+      },
+      data: {
+        requiresLogin: true
       }
     })
-
+    .state('app.history', {
+      url: '/history',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/history.html'
+        }
+      },
+      data: {
+        requiresLogin: true
+      }
+    })
   .state('app.single', {
     url: '/playlists/:playlistId',
     views: {
@@ -75,8 +109,17 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         templateUrl: 'templates/playlist.html',
         controller: 'PlaylistCtrl'
       }
+    },
+    data: {
+      requiresLogin: true
     }
   });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/search');
+
+  authProvider.init({
+    domain: 'plzhelp.auth0.com',
+    clientID: 'xWmflzgefqaDNoeY7t1EUGOoMdNEwQKG',
+    loginState: 'login' // This is the name of the state where you'll show the login, which is defined above...
+  });
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -83,4 +83,28 @@ angular.module('starter.controllers', [])
       // $scope.userData = appFact.userData;
       // $state.go('index.list.overview');
     });
+})
+.controller('LoginCtrl', function($scope, auth, $state, store) {
+ auth.signin({
+   authParams: {
+     // This asks for the refresh token
+     // So that the user never has to log in again
+     scope: 'openid offline_access',
+     // This is the device name
+     device: 'Mobile device'
+   },
+   // Make the widget non closeable
+   standalone: true
+ }, function(profile, token, accessToken, state, refreshToken) {
+         // Login was successful
+   // We need to save the information from the login
+   store.set('profile', profile);
+   store.set('token', token);
+   store.set('refreshToken', refreshToken);
+   $state.go('app.profile');
+ }, function(error) {
+   // Oops something went wrong during login:
+   console.log("There was an error logging in", error);
+ });
 });
+
